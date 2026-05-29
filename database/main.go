fd9fa1f63c968aa7de29a9f4db39e88a5f1de622
GitHub Carry-Rao <GitHub carry3658@outlook.com>
2026-05-30 00:49:33 +0800

    Operation: Merge, Module: database: Provide a unity init function.

---
+++ database/main.go
@@ -0,0 +1,36 @@
+package database
+
+import (
+	"errors"
+
+	"github.com/Carry-Rao/goutils/database/api"
+
+	"github.com/Carry-Rao/goutils/database/bloom"
+	"github.com/Carry-Rao/goutils/database/memory"
+	"github.com/Carry-Rao/goutils/database/mixture"
+	"github.com/Carry-Rao/goutils/database/mysql"
+	"github.com/Carry-Rao/goutils/database/postgresql"
+	"github.com/Carry-Rao/goutils/database/redis"
+	"github.com/Carry-Rao/goutils/database/sqlite"
+)
+
+func NewDatabase(config map[string]string) (api.Database, error) {
+	typ := config["type"]
+	switch typ {
+	case "redis":
+		return redis.NewDatabase(config)
+	case "memory":
+		return memory.NewDatabase(config)
+	case "mysql":
+		return mysql.NewDatabase(config)
+	case "sqlite":
+		return sqlite.NewDatabase(config)
+	case "postgresql":
+		return postgresql.NewDatabase(config)
+	case "bloom":
+		return bloom.NewDatabase(config)
+	case "mixture":
+		return mixture.NewDatabase(config)
+	}
+	return nil, errors.New("Unknown database type.")
+}
