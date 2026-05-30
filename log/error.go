e84531f304f24d5ceecd152f4e29c29f7d24e259
GitHub Carry-Rao <GitHub carry3658@outlook.com>
2026-05-30 14:41:09 +0800

    Operator: Fix, Module: Log: Compare and correct log levels

--- log/error.go
+++ log/error.go
@@ -1,7 +1,7 @@
 package log
 
 func (l *Logger) Error(info string) {
-	if l.LogLevel >= Error {
+	if l.LogLevel <= Error {
 		if l.Color {
 			l.addLog(Log{Error, "\033[31m[ERROR]\033[0m" + info + "\n"})
 		} else {
